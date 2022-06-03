6d244df311ebf182002843e4e70ef9ac66fea46f
jorddo <jordanbsneed@gmail.com>
2022-06-03 16:00:48 -0500

    importing path

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
-// const path = require('path');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
